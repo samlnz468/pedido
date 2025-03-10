6949cc9399b81ade5c0de45cb7711b16362c5d77
samlnz468 <mukalinosss@gmail.com>
2025-03-10 02:29:25 -0300

    Add files via upload

---
+++ pedido/script.js
@@ -0,0 +1,15 @@
+function sim() {
+    alert("Você tomou a melhor decisão")
+}
+
+function desvia(t) {
+    var btn = t;
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log("Você não tem escolha >:(")
+}
+
+function geraPosicao(min, max) {
+return Math.random() * (max - min) + "%";
+}
